65650dc6747e33479e208b437f32a02b7ec0ace5
DP3PO <williampferebee@gmail.com>
2019-11-12 16:40:27 -0500

    main project done, need to update read me and iron out kinks

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Meeses');
+INSERT INTO burgers (name) VALUES ('Bobbi');
+INSERT INTO burgers (name) VALUES ('Sylvester');
+INSERT INTO burgers (name) VALUES ('Marilyn Manson');
+INSERT INTO burgers (name) VALUES ('Joe Biden');
+INSERT INTO burgers (name) VALUES ('Owens');
