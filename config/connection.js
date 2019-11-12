a9413d9d564184dd4294ecacc35ad1b2ba3511f5
DP3PO <williampferebee@gmail.com>
2019-11-12 16:55:23 -0500

    troublehooting heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "127.0.0.1",
+  host: "localhost",
   port: 3306,
   user: "root",
-  password: "your new password",
+  password: "",
   database: "burgers_db"
 });
 
